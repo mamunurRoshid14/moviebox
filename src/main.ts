661c30eacf8cfb6fefd5205711c06abac0e07713
Mamunur Rashid <mamun@kaz.com.bd>
2025-07-07 16:45:01 +0600

    feature 1 done dev mode displaying log but production mode not

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+
+  // Disable console methods in production
+  console.log = function () {};
+  console.info = function () {};
+  console.warn = function () {};
+  console.error = function () {};
+  console.debug = function () {};
+  console.trace = function () {};
+}
 
 bootstrapApplication(AppComponent)
   .catch(err => console.error(err));
