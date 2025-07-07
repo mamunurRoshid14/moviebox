6f9b2e77bb1b5837351982317e2c01eaae82f280
Mamunur Rashid <mamun@kaz.com.bd>
2025-07-07 19:09:05 +0600

    data displaying in console using proxy server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html', // 👈 Using external HTML
+  imports: [HttpClientModule],
+  templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    const url = '/api/movie/changes?page=1';
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNTYyNjViMTA3NzFkNzI3Nzk4NDQzZWE2NWExNzZmNyIsIm5iZiI6MTc1MTg4NjY3Ny4zNTgsInN1YiI6IjY4NmJhYjU1YzhlYzE3NDVhM2NlZTc4ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.W9FAPBH8zc7TMJ-WMvq1TYsESJs_l9OoQCz0Ptpj8AY',
+      'accept': 'application/json'
+    });
+
+    this.http.get(url, { headers }).subscribe({
+      next: (response) => console.log('TMDb API response:', response),
+      error: (error) => console.error('Error fetching TMDb data:', error)
+    });
+  }
+
   logMessages(): void {
     console.log('🔵 This is a log message.');
     console.info('🟢 This is an info message.');
@@ -14,4 +32,6 @@ export class AppComponent {
     console.debug('⚪ This is a debug message.');
     console.trace('🧭 This is a trace message.');
   }
+  title="Myapp"
 }
+
